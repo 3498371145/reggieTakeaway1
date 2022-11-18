bfe96a7dcca209de29e683bd1453f7f661f95017
zy-li <3498371145@qq.com>
2022-11-18 11:10:23 +0800

    use redis

--- src/main/java/com/reggie/controller/userController.java
+++ src/main/java/com/reggie/controller/userController.java
@@ -9,11 +9,13 @@ import com.reggie.service.userService;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 @RestController
@@ -21,6 +23,8 @@ import java.util.Map;
 public class userController {
     @Autowired
     private userService userService;
+    @Autowired
+    private RedisTemplate redisTemplate;
 
     @PostMapping("/sendMsg")
     public R<String> sendMsg(@RequestBody User user, HttpSession session){
@@ -29,8 +33,10 @@ public class userController {
             String code = ValidateCodeUtils.generateValidateCode(4).toString();
             log.info("code is {}",code);
             //SMSUtils.sendMessage("阿里云短信测试","",phone,code);
-            session.setAttribute("phone",phone);
-            session.setAttribute("code",code);
+//            session.setAttribute("phone",phone);
+//            session.setAttribute("code",code);
+            //将生成验证码缓存到redis
+            redisTemplate.opsForValue().set(phone,code,5, TimeUnit.MINUTES);
             return R.success("手机验证码发送成功");
         }
         return R.error("手机验证码发送失败");
@@ -38,9 +44,13 @@ public class userController {
 
     @PostMapping("/login")
     public R<User> login(@RequestBody Map map, HttpSession session){
+        //从redis获取验证码
+
         String phone = map.get("phone").toString();
         String code = map.get("code").toString();
-        Object codeInSession = session.getAttribute("code");
+        log.info(code);
+        //Object codeInSession = session.getAttribute("code");
+        Object codeInSession = redisTemplate.opsForValue().get(phone);
         if (codeInSession!=null&&codeInSession.equals(code)){
             LambdaQueryWrapper<User> wrapper = new LambdaQueryWrapper<>();
             wrapper.eq(User::getPhone,phone);
@@ -51,6 +61,8 @@ public class userController {
                 userService.save(user);
             }
             session.setAttribute("user",user.getId());
+            //登录成功删除验证码
+            redisTemplate.delete(phone);
             return  R.success(user);
         }
         return R.error("登陆失败");
--- src/test/java/t1.java
+++ src/test/java/t1.java
@@ -1,11 +1,17 @@
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.StringRedisTemplate;
 
 import java.time.LocalDateTime;
 import java.util.Date;
-
 public class t1 {
+    @Autowired
+    private StringRedisTemplate redisTemplate;
     @Test
     public void t1(){
         System.out.println(LocalDateTime.now());
     }
+
 }
